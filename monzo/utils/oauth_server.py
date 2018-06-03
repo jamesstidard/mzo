a6fbd352aaa6ae1283ae483a625c693f19e0f73d
James Stidard <james@stidard.com>
2018-06-03 18:03:11 +0100

    oauth via middle man server

--- monzo/utils/oauth_server.py
+++ monzo/utils/oauth_server.py
@@ -2,14 +2,11 @@ from asyncio import Event
 
 from urllib.parse import urlencode
 
-import aiohttp
-
 from sanic import Sanic
 from sanic.response import text
 from sanic.exceptions import Unauthorized
 
-CLIENT_ID = 'oauthclient_00009QgEkW8zP76s4rwEDZ'
-CLIENT_SECRET = 'nf8XwQJa/Tx87EU8pny5OUTHtAf4jch6fv9XxRCn/aRdsEUU02EcTow6+Cod+fJ2VvI7B9UMGWh6sozJgamZ'
+CLIENT_ID = 'oauth2client_00009XGzCVTlL1OWLEsMnx'
 REDIRECT_URI = 'http://localhost:40004/welcome-back'
 RESPONSE_TYPE = 'code'
 
@@ -24,14 +21,13 @@ class OAuthServer:
         self._access_token = None
 
         @self.app.route("/welcome-back")
-        async def test(request):
+        async def welcome_back(request):
             if request.args['state'][0] != self.nonce:
                 raise Unauthorized("The nonce returned from Monzo does not match the one sent out.")
 
-            resp = await self.http.post('https://api.monzo.com/oauth2/token', data={
+            resp = await self.http.post('https://monzo-cli.herokuapp.com/oauth2/token', data={
                 'grant_type': 'authorization_code',
                 'client_id': CLIENT_ID,
-                'client_secret': CLIENT_SECRET,
                 'redirect_uri': REDIRECT_URI,
                 'code': request.args['code'][0]})
             self._access_token = await resp.json()
