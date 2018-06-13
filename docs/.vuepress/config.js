7fc171cd635f8b30fcf99fd2918a938a6cbff891
James Stidard <james@stidard.com>
2018-06-13 21:27:11 +0100

    no base url for cnamed github docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Monzo CLI',
   description: 'Sometimes typing is more convenient then swiping and tapping... Sometimes.',
   ga: 'UA-33148627-2',
-  base: '/Monzo-Cli/', // For github pages
+  // base: '/Monzo-Cli/', // For github pages. Not needed for custom domain
   themeConfig: {
     nav: [
       { text: 'Documentation', link: '/docs/' },
