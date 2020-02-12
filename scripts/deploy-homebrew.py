2bb33b1e9fcd7cd91eef4a8229ecddeabe756bc6
James Stidard <james@stidard.com>
2020-02-12 22:50:26 +0000

    black

--- scripts/deploy-homebrew.py
+++ scripts/deploy-homebrew.py
@@ -52,66 +52,81 @@ def env(dict_):
 here = os.path.abspath(os.path.dirname(__file__))
 about = {}
 
-with open(os.path.join(here, '..', 'mzo', '__version__.py')) as f:
+with open(os.path.join(here, "..", "mzo", "__version__.py")) as f:
     exec(f.read(), about)
 
-mzo_version = about['__version__']
+mzo_version = about["__version__"]
 
 with TemporaryDirectory() as td:
     # Download current release source from git tag
-    tar_release_url = f'https://github.com/jamesstidard/Mzo-Cli/archive/v{mzo_version}.tar.gz'
-    tar_release_path = os.path.join(td, 'release.tar.gz')
+    tar_release_url = (
+        f"https://github.com/jamesstidard/Mzo-Cli/archive/v{mzo_version}.tar.gz"
+    )
+    tar_release_path = os.path.join(td, "release.tar.gz")
     os.system(f'curl --location "{tar_release_url}" --output {tar_release_path}')
 
-    with open(tar_release_path, 'rb') as fp:
+    with open(tar_release_path, "rb") as fp:
         tar_release_sha256 = hashlib.sha256(fp.read()).hexdigest()
 
     # Extract to td
-    with tarfile.open(tar_release_path, 'r:gz') as tar:
+    with tarfile.open(tar_release_path, "r:gz") as tar:
         source_path = os.path.join(td, os.path.commonprefix(tar.getnames()))
         tar.extractall(path=td)
 
     # Init dependencies with pipenv
     with change_dir(source_path):
         # Ignore development pipenv environment and make a new one for release
-        with env({'PIPENV_IGNORE_VIRTUALENVS': '1', 'PIPENV_VENV_IN_PROJECT': '1', 'LANG': 'en_US.utf8'}):
-            os.system('pipenv install --dev')
+        with env(
+            {
+                "PIPENV_IGNORE_VIRTUALENVS": "1",
+                "PIPENV_VENV_IN_PROJECT": "1",
+                "LANG": "en_US.utf8",
+            }
+        ):
+            os.system("pipenv install --dev")
 
             # wait for mzo to be available on pypi
-            print('Waiting for release to be visible on PyPI...')
+            print("Waiting for release to be visible on PyPI...")
             while True:
                 try:
-                    urllib.request.urlopen(f'https://pypi.org/project/mzo/{mzo_version}/')
+                    urllib.request.urlopen(
+                        f"https://pypi.org/project/mzo/{mzo_version}/"
+                    )
                 except urllib.error.HTTPError:
                     time.sleep(5)
                 else:
                     break
 
-            resources = subprocess.check_output('pipenv run poet mzo', shell=True).decode('ascii')
+            resources = subprocess.check_output(
+                "pipenv run poet mzo", shell=True
+            ).decode("ascii")
             # remove mzo so homebrew detects mzo as a new binary
-            resources = re.sub(' {2}resource "mzo".+?end', '', resources, flags=re.DOTALL)
+            resources = re.sub(
+                ' {2}resource "mzo".+?end', "", resources, flags=re.DOTALL
+            )
 
     # Update Homebrew tap repo
-    homebrew_repo = os.path.join(td, 'homebrew-formulas')
-    os.system(f'git clone git@github.com:jamesstidard/homebrew-formulas "{homebrew_repo}"')
+    homebrew_repo = os.path.join(td, "homebrew-formulas")
+    os.system(
+        f'git clone git@github.com:jamesstidard/homebrew-formulas "{homebrew_repo}"'
+    )
     with change_dir(homebrew_repo):
         tap = TAP_TEMPLATE.format(
-            url=tar_release_url,
-            sha256=tar_release_sha256,
-            resources=resources)
+            url=tar_release_url, sha256=tar_release_sha256, resources=resources
+        )
 
-        with open('mzo.rb', 'w') as fp:
+        with open("mzo.rb", "w") as fp:
             fp.write(tap)
 
         now = datetime.utcnow()
         start = datetime(now.year, now.month, now.day)
-        delta = (now-start).total_seconds()
-        repo_version = f'v{now:%Y.%m.%d}.{delta:.0f}'
+        delta = (now - start).total_seconds()
+        repo_version = f"v{now:%Y.%m.%d}.{delta:.0f}"
 
-        os.system('git add .')
+        os.system("git add .")
         os.system(f'git commit -m "mzo v{mzo_version}"')
-        os.system('git push origin master')
-        os.system(f'git tag {repo_version}')
-        os.system(f'git push origin {repo_version}')
+        os.system("git push origin master")
+        os.system(f"git tag {repo_version}")
+        os.system(f"git push origin {repo_version}")
 
-print('Homebrew tap updated')
+print("Homebrew tap updated")
